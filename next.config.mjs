43764607d901b9a39302072dfe3c5d97115a980e
PHunt14 <PHunt14@users.noreply.github.com>
2026-02-22 15:26:44 -0500

    test: the square access token 00

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    SQUARE_ACCESS_TOKEN: process.env.SQUARE_ACCESS_TOKEN,
+  },
+}
 
 export default nextConfig
