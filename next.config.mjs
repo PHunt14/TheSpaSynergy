5f29783cf7260423bc6db4eb5371937a6e552f4e
PHunt14 <PHunt14@users.noreply.github.com>
2026-03-18 20:16:44 -0400

    fix: the test emails

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   env: {
     SQUARE_ACCESS_TOKEN: process.env.SQUARE_ACCESS_TOKEN,
+    EMAIL_PROVIDER: process.env.EMAIL_PROVIDER,
+    EMAIL_TEST_ADDRESS: process.env.EMAIL_TEST_ADDRESS,
+    SES_FROM_EMAIL: process.env.SES_FROM_EMAIL,
+    SMS_PROVIDER: process.env.SMS_PROVIDER,
+    SMS_TEST_PHONE: process.env.SMS_TEST_PHONE,
+    SNS_ORIGINATION_NUMBER: process.env.SNS_ORIGINATION_NUMBER,
   },
   images: {
     remotePatterns: [
