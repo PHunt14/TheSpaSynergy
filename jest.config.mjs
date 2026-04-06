a4f787689ae992a48d4a782566f32b7ba2b89537
PHunt14 <PHunt14@users.noreply.github.com>
2026-04-05 20:56:58 -0400

    fix: tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,6 +3,7 @@ const config = {
   testEnvironment: 'node',
   transform: {},
   testMatch: ['**/__tests__/**/*.test.mjs'],
+  testPathIgnorePatterns: ['/node_modules/', '/services/'],
 }
 
 export default config
